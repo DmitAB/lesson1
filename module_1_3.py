515b61148dd7c8c5ed351d821c09cd57150eef64
Dmitriy <zhilexpert@gmail.com>
2024-11-15 09:30:35 +0400

    Add files via upload

--- module_1_3.py
+++ module_1_3.py
@@ -6,3 +6,8 @@ age=age+1  # Здесь можно ещё age=(40+1) или сразу print(age
 print(age)
 is_student=True
 print(is_student)
+homework=12
+hours=1.5
+name_course="Python"
+one_task=1.5/12
+print(name_course,", всего задач:",(homework),", затрачено часов:",(hours)," среднее время выполнения",(one_task))
