f1785697c6c5587bf52d66f6f6288b67a177905c
Dmitriy <zhilexpert@gmail.com>
2024-11-15 20:59:11 +0400

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,7 @@
+my_string=input('Вы любите оливки? ')
+print(len(my_string)) # вывести количество символов введенного текста - возможно я забыл, но, кажется, раньше не возникало такой задачи, пришлось узнавать самостоятельно.
+print(my_string.upper())
+print(my_string.lower())
+print(my_string.replace(' ',''))
+print(my_string[0])
+print(my_string[-1])
